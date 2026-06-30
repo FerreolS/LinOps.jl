fc5aae04bd8194dc61e191f7db667226f244844a
Ferréol Soulez <ferreol.soulez@univ-lyon1.fr>
2026-06-30 16:39:07 +0200

    feat: add verify_adjoint function for LinOp verification

--- src/utils.jl
+++ src/utils.jl
@@ -2,3 +2,9 @@ _dims2tuple(dims::Integer) = (Int(dims),)
 _dims2tuple(dims::NTuple{N, <:Integer}) where {N} = dims
 _dims2tuple(dims::SVector{N, <:Integer}) where {N} = ntuple(i -> Int(dims[i]), Val(N))
 _dims2tuple(dims::AbstractVector{<:Integer}) = Tuple(Int.(dims))
+
+function verify_adjoint(A::LinOp)
+	x = randn(inputspace(A))
+	y = randn(outputspace(A))
+	dot(y, A*x)  ≈ dot(A'y, x) 
+end
